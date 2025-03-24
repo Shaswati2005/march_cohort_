6ea831583530e05dd5a75d5afca9f6d33322d404
Shaswati2005 <mishrashaswati2005@gmail.com>
2025-03-25 00:39:50 +0530

    navbar changes

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 left-0 w-screen z-10'>
+    <div className='fixed top-0 left-0 w-screen z-30'>
         <div className='hidden  md:flex flex-row items-center justify-between px-10 py-1 lg:h-20   bg-white inset-0  bg-opacity-50 '>
             <div>logo</div>
             <div className='flex flex-1 px-10 py-3 items-center gap-10 text-[#8bc34a]'>
