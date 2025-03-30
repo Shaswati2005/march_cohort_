591f7e6e6ef6bbff2f671eb7a4d365d5074f18bc
dasbidyendu <143386290+dasbidyendu@users.noreply.github.com>
2025-03-30 06:47:41 +0530

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="fixed top-0 left-0 w-screen z-30 lg:text-xl  bg-[#c38f4a8a] font-bold font-stretch-normal">
+    <div className="fixed top-0 left-0 w-screen z-30 lg:text-xl  bg-[#f99b1f8a] font-bold font-stretch-normal">
       <div className="flex lg:hidden flex-row items-center justify-between px-4 py-2  bg-opacity-50">
         <div>logo</div>
         <button onClick={toggleMenu} className="text-[#ffffff]">
@@ -23,33 +23,55 @@ const Navbar = () => {
       <div className="hidden lg:flex flex-row items-center justify-between px-10 py-1 lg:h-20 bg-opacity-50">
         <div>logo</div>
         <div className="flex flex-1 w-full px-10 py-3 items-center gap-10 text-[#ffffff]">
-          <Link href={"/"} title="home " className="relative group  hover:scale-110 transition-all duration-300">
+          <Link
+            href={"/"}
+            title="home "
+            className="relative group  hover:scale-110 transition-all duration-300"
+          >
             Home
-            <div className="absolute bottom-0 left-0 w-0 h-1 bg-[#c38f4a] transition-all duration-300 ease-in-out group-hover:w-full "></div>
+            <div className="absolute bottom-0 left-0 w-0 h-[3px]  bg-[#ffffff]  transition-all duration-300 ease-in-out group-hover:w-full "></div>
           </Link>
-          <button className="relative group  hover:scale-110 transition-all duration-300">About us
-          <div className="absolute bottom-0 left-0 w-0 h-1 bg-[#c38f4a] transition-all duration-300 ease-in-out group-hover:w-full"></div>
+          <button className="relative group  hover:scale-110 transition-all duration-300">
+            About us
+            <div className="absolute bottom-0 left-0 w-0 h-[3px] bg-[#ffffff] transition-all duration-300 ease-in-out group-hover:w-full"></div>
           </button>
-          
-          <button className="relative group  hover:scale-110 transition-all duration-300">Destination
-          <div className="absolute bottom-0 left-0 w-0 h-1 bg-[#c38f4a] transition-all duration-300 ease-in-out group-hover:w-full"></div>
+
+          <button className="relative group  hover:scale-110 transition-all duration-300">
+            Destination
+            <div className="absolute bottom-0 left-0 w-0 h-[3px] bg-[#ffffff] transition-all duration-300 ease-in-out group-hover:w-full"></div>
           </button>
-          <Link href={"/"} title="past " className="relative group  hover:scale-110 transition-all duration-300">
+          <Link
+            href={"/"}
+            title="past "
+            className="relative group  hover:scale-110 transition-all duration-300"
+          >
             Past tours
-            <div className="absolute bottom-0 left-0 w-0 h-1 bg-[#c38f4a] transition-all duration-300 ease-in-out group-hover:w-full"></div>
+            <div className="absolute bottom-0 left-0 w-0 h-[3px] bg-[#ffffff] transition-all duration-300 ease-in-out group-hover:w-full"></div>
           </Link>
 
-          <Link href={"/"} title="past " className="relative group  hover:scale-110 transition-all duration-300">
+          <Link
+            href={"/"}
+            title="past "
+            className="relative group  hover:scale-110 transition-all duration-300"
+          >
             Bookings
-            <div className="absolute bottom-0 left-0 w-0 h-1 bg-[#c38f4a] transition-all duration-300 ease-in-out group-hover:w-full"></div>
+            <div className="absolute bottom-0 rounded-xl left-0 w-0 h-[3px] bg-[#ffffff] transition-all duration-300 ease-in-out group-hover:w-full"></div>
           </Link>
-          <Link href={"/"} title="updates" className="relative group  hover:scale-110 transition-all duration-300">
+          <Link
+            href={"/"}
+            title="updates"
+            className="relative group  hover:scale-110 transition-all duration-300"
+          >
             Updates
-            <div className="absolute bottom-0 left-0 w-0 h-1 bg-[#c38f4a] transition-all duration-300 ease-in-out group-hover:w-full"></div>
+            <div className="absolute bottom-0 left-0 w-0 h-[3px] bg-[#ffffff] transition-all duration-300 ease-in-out group-hover:w-full"></div>
           </Link>
-          <Link href={`${user?.id}`} title="current" className="relative group  hover:scale-110 transition-all duration-300">
+          <Link
+            href={`${user?.id}`}
+            title="current"
+            className="relative group  hover:scale-110 transition-all duration-300"
+          >
             Upcoming Trips
-            <div className="absolute bottom-0 left-0 w-0 h-1 bg-[#c38f4a] transition-all duration-300 ease-in-out group-hover:w-full"></div>
+            <div className="absolute bottom-0 left-0 w-0 h-[3px] bg-[#ffffff] transition-all duration-300 ease-in-out group-hover:w-full"></div>
           </Link>
         </div>
         {isSignedIn ? (
@@ -57,7 +79,7 @@ const Navbar = () => {
         ) : (
           <Link
             href={"/sign-in"}
-            className="bg-[#c38f4a] px-5 py-1 rounded-lg text-xl"
+            className="bg-[#ffffff] px-5 py-1 rounded-lg text-xl"
           >
             Sign In
           </Link>
@@ -66,27 +88,56 @@ const Navbar = () => {
 
       {isMenuOpen && (
         <div className="lg:hidden  bg-opacity-50 p-4">
-          <div className="flex flex-col absolute right-2 items-center gap-4 p-4 max-w-[400px] w-full bg-[#c38f4a] text-white">
+          <div className="flex flex-col absolute right-2 items-center gap-4 p-4 max-w-[400px] w-full bg-[#ffffff] text-white">
             {isSignedIn ? (
               <UserButton userProfileMode="modal" />
             ) : (
-              <Link href={'/app/(auth)/sign-in'} className="bg-[#c38f4a] px-5 py-1 rounded-lg hover:bg-[#c38f4a9a]">signin</Link>
+              <Link
+                href={"/app/(auth)/sign-in"}
+                className="bg-[#ffffff] px-5 py-1 rounded-lg hover:bg-[#f99b1f9a]"
+              >
+                signin
+              </Link>
             )}
-            <Link href={"/"} title="home" className="hover:bg-[#c3704a4c] w-full h-fit text-center">
+            <Link
+              href={"/"}
+              title="home"
+              className="hover:bg-[#c3704a4c] w-full h-fit text-center"
+            >
               Home
             </Link>
-            <button  className="hover:bg-[#c3704a4c] w-full h-fit text-center">About us</button>
-            <button  className="hover:bg-[#c3704a4c] w-full h-fit text-center">Destination</button>
-            <Link href={"/"} title="past "  className="hover:bg-[#c3704a4c] w-full h-fit text-center">
+            <button className="hover:bg-[#c3704a4c] w-full h-fit text-center">
+              About us
+            </button>
+            <button className="hover:bg-[#c3704a4c] w-full h-fit text-center">
+              Destination
+            </button>
+            <Link
+              href={"/"}
+              title="past "
+              className="hover:bg-[#c3704a4c] w-full h-fit text-center"
+            >
               Past tours
             </Link>
-            <Link href={"/"} title="home" className="hover:bg-[#c3704a4c] w-full h-fit text-center">
+            <Link
+              href={"/"}
+              title="home"
+              className="hover:bg-[#c3704a4c] w-full h-fit text-center"
+            >
               Bookings
             </Link>
-            <Link href={"/"} title="updates "  className="hover:bg-[#c3704a4c] w-full h-fit text-center">
+            <Link
+              href={"/"}
+              title="updates "
+              className="hover:bg-[#c3704a4c] w-full h-fit text-center"
+            >
               Updates
             </Link>
-            <Link href={`${user?.id}`} title="current"  className="hover:bg-[#c3704a4c] w-full h-fit text-center">
+            <Link
+              href={`${user?.id}`}
+              title="current"
+              className="hover:bg-[#c3704a4c] w-full h-fit text-center"
+            >
               Upcoming Trips
             </Link>
           </div>
